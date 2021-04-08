ebadf5e187cff3bfe9f1952257bcc8573f2e4368
samarthp07 <82090424+samarthp07@users.noreply.github.com>
2021-04-08 14:09:50 +0530

    Add files via upload

---
+++ circle_sam.py
@@ -0,0 +1,9 @@
+#python 3.7.1
+
+print ("code for:- Area of circle!")
+print ("______________________")
+radius = input("Enter radius of circle.")
+radius = float (radius)
+print ("radius:=" , radius)
+print ("Area of circle:-", 3.15*radius*radius )
+print ("************************")
